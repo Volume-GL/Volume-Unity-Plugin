f15a21631925cbeb48f7c052deb796f74edb3fd8
Or Fleisher <fleisher.or@gmail.com>
2018-03-20 13:25:06 -0400

    Init

---
+++ Assets/Volume/Scripts/VolumeGeometry.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class VolumeGeometry : MonoBehaviour {
+
+    //Top bottom texture slot
+    public Texture VolumeTexture;
+
+    //Reference to the Volume shader
+    public Shader VolumeShader;
+
+    //Mesh density dropdown menu
+    public enum Density {
+        Medium, Low
+    }
+    public Density _meshDensity = Density.Medium;
+
+    //Drawing style
+    public enum DrawingStyle{
+        Points
+    }
+    public DrawingStyle _drawingStyle = DrawingStyle.Points;
+
+    //Private mesh width, height variables
+    private int xSize, ySize;
+
+
+	private void Awake () {
+		Generate();
+        createMaterial();
+	}
+
+    private void calculateDensity () {
+        switch(_meshDensity){
+            case Density.Medium:
+                xSize = 256;
+                ySize = 256;
+                break;
+            case Density.Low:
+                xSize = 128;
+                ySize = 128;
+                break;
+            default:
+                xSize = 256;
+                ySize = 256;
+                break;
+        }
+    }
+
+	private void Generate () {
+
+        Mesh mesh;
+        Vector3[] vertices;
+
+        calculateDensity();
+
+        mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+        //mesh = 
+        mesh.bounds = new Bounds(Vector3.zero, 10000f * Vector3.one);
+        mesh.name = "Volume Grid";
+
+		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		Vector2[] uv = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+		for (int i = 0, y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++, i++) {
+				vertices[i] = new Vector3(x, y);
+				uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+				tangents[i] = tangent;
+			}
+		}
+		mesh.vertices = vertices;
+		mesh.uv = uv;
+		mesh.tangents = tangents;
+
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals();
+
+        //Set the drawing mesh topolgy
+        mesh.SetIndices(mesh.triangles, MeshTopology.Points, 0);
+
+	}
+
+	private void createMaterial(){
+        
+        Renderer rend = GetComponent<Renderer>();
+        rend.sharedMaterial = new Material(VolumeShader);
+        rend.sharedMaterial.mainTexture = VolumeTexture;
+        rend.sharedMaterial.SetFloat("_Displacement", 100.0f);
+
+    }
+}
---
+++ Assets/Volume/Shaders/Volume.shader
@@ -0,0 +1,77 @@
+﻿Shader "Volume/Volume Unlit"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+        _Displacement("Displacement", Range(0.0, 500.0)) = 5.0
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 600
+        Cull Off
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+            //Uniforms
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+            float _Displacement;
+
+            //Vertex shader
+			v2f vert (appdata v)
+			{   
+                //Split the UV so we only use the lower half
+                fixed4 depthUV = fixed4(v.uv.x, v.uv.y, 0, 0);
+                depthUV.y *= 0.5;
+
+                //Sample the depth map
+                fixed4 depthSample = tex2Dlod(_MainTex, depthUV);
+                v.vertex.z -= (-depthSample.r * _Displacement);
+                //Pack all the data for the fragment shader
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+
+            //Fragment shader
+			fixed4 frag (v2f i) : SV_Target
+			{
+                //Split the UV's so we read the upper half
+                fixed2 colorUV = i.uv;
+                colorUV.y *= 0.5;
+                colorUV.y += 0.5;
+
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, colorUV);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+            
+			ENDCG
+		}
+	}
+}
